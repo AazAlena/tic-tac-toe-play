37d46daa52e9c08078afb9953d71620f68380f05
Ruchkina <ruch.nastya@gmail.com>
2023-10-06 21:20:39 +0300

    remake win

--- js/index.js
+++ js/index.js
@@ -8,29 +8,39 @@ let cont = document.querySelector(`.cont`);
 function print(coord, count){
     if (count%2==1){
         coord.innerHTML = "X";
-
+        
         matrix[coord.id-1] = 1;
-    } else {
+        // console.log( "2", matrix);
+    } else if (count%2==0) {
         coord.innerHTML = "0";
-
-        matrix[coord.id] = 0;
+        
+        matrix[coord.id-1] = 0;
     }
+    // console.log( "3", matrix);
 }
 
 cont.addEventListener('click', function(evt){
     coord = evt.target;
     console.log(coord);
-    if (matrix[coord.id] == null){
-        count+=1;
+    console.log(count, "1", matrix)
+    if (matrix[coord.id-1] == null){
+        count=count+1;
         print(coord, count);
-        console.log(matrix)
+        console.log( "4", matrix);
         if (count==9){
             alert(`ничья`);
-        } else if ((matrix[1] == matrix[2] == matrix[3] == 1) /  (matrix[4] == matrix[5] == matrix[6] == 1)/  (matrix[7] == matrix[8] == matrix[9]== 1)/  (matrix[1] == matrix[4] == matrix[7]== 1)/  (matrix[2] == matrix[5] == matrix[8]==1)/  (matrix[3] == matrix[9] == matrix[6]== 1)/  (matrix[1] == matrix[5] == matrix[9]== 1)/  (matrix[3] == matrix[5] == matrix[7]== 1) ){
+            // console.log( "6.1", matrix);
+        } else if ((matrix[0] == matrix[1] == matrix[2] == 1) /  (matrix[3] == matrix[4] == matrix[5] == 1)/  (matrix[6] == matrix[7] == matrix[8]== 1)/  (matrix[0] == matrix[3] == matrix[7]== 1)/  (matrix[1] == matrix[4] == matrix[7]==1)/  (matrix[2] == matrix[8] == matrix[5]== 1)/  (matrix[0] == matrix[4] == matrix[8]== 1)/  (matrix[2] == matrix[4] == matrix[6]== 1) ){
             alert(`Выиграли крестики`);
-        } else if ((matrix[1] = matrix[2] = matrix[3] = 0) /  (matrix[4] = matrix[5] = matrix[6] = 0)/  (matrix[7] = matrix[8] = matrix[9]= 0)/  (matrix[1] = matrix[4] = matrix[7]= 0)/  (matrix[2] = matrix[5] = matrix[8]= 0)/  (matrix[3] = matrix[9] = matrix[6]= 0)/  (matrix[1] = matrix[5] = matrix[9]= 0)/  (matrix[3] = matrix[5] = matrix[7]= 0) ){
+            // console.log( "6.2", matrix);
+        } else if ((matrix[0] == matrix[1] == matrix[2] == 0) /  (matrix[3] == matrix[4] == matrix[5] == 0)/  (matrix[6] == matrix[7] == matrix[8]== 0)/  (matrix[0] == matrix[3] == matrix[7]== 0)/  (matrix[1] == matrix[4] == matrix[7]==0)/  (matrix[2] == matrix[8] == matrix[5]== 0)/  (matrix[0] == matrix[4] == matrix[8]== 0)/  (matrix[2] == matrix[4] == matrix[6]== 0) ){
             alert(`Выиграли нолики`);
-        }
+            // console.log( "6.3", matrix);
+        } else {
+            console.log("подолжить")
+        };
+        // console.log( "5", matrix);
+        console.log(null==0);
     }
 
     }
