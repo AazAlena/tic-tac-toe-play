ea2d55d7413c6e3a860c2448745c937d655f3382
Ruchkina <ruch.nastya@gmail.com>
2023-10-10 16:16:49 +0300

    super

--- js/index.js
+++ js/index.js
@@ -1,5 +1,9 @@
 let count = 0;
-let matrix = []
+let matrix = [];
+let cont = document.querySelector(`.cont`);
+let again = document.querySelector(`.again`);
+
+// создание матрицы
 for (i=0; i<3; i++){
     let list = [];
     
@@ -8,67 +12,58 @@ for (i=0; i<3; i++){
     }
     matrix.push(list);
 }
-console.log(matrix)
-let cont = document.querySelector(`.cont`);
-// let a = NaN;
-// let b = NaN;
-// let c = NaN;
-// console.log("йцу", c + a + b, matrix);
-        
-function print(coord, count){
+// функция печати к\н и добавления их в matrix 
+function print(coord, count, x, y){
     if (count%2 == 1){
         coord.innerHTML = `<img src="assets/cross.png" class="sing" alt="">`;
-        matrix[coord.id-1] = 1;
-        // console.log( "2", matrix);
+        matrix[x][y] = 1;
     } else {
         coord.innerHTML = `<img src="assets/round.png" class="sing" alt="">`;
-        matrix[coord.id-1] = 0;
+        matrix[x][y] = 0;
     }
-    // console.log( "3", matrix);
+}
+// функция проверки выигрыша
+function test_win(matrix){
+    if ((matrix[0][0] + matrix[0][1] + matrix[0][2] === 0) ||  
+        (matrix[1][0] + matrix[1][1] + matrix[1][2] === 0) ||  
+        (matrix[2][0] + matrix[2][1] + matrix[2][2] === 0) || 
+        (matrix[0][0] + matrix[1][0] + matrix[2][0] === 0) || 
+        (matrix[0][1] + matrix[1][1] + matrix[2][1] === 0) || 
+        (matrix[0][2] + matrix[1][2] + matrix[2][2] === 0) || 
+        (matrix[0][0] + matrix[1][1] + matrix[2][2] === 0) || 
+        (matrix[0][2] + matrix[1][1] + matrix[2][0] === 0) )
+        {
+            setTimeout(function(){alert("Выиграли нолики")}, 100);
+        } 
+    else if ((matrix[0][0] + matrix[0][1] + matrix[0][2] === 3) ||  
+            (matrix[1][0] + matrix[1][1] + matrix[1][2] === 3) ||  
+            (matrix[2][0] + matrix[2][1] + matrix[2][2] === 3) || 
+            (matrix[0][0] + matrix[1][0] + matrix[2][0] === 3) || 
+            (matrix[0][1] + matrix[1][1] + matrix[2][1] === 3) || 
+            (matrix[0][2] + matrix[1][2] + matrix[2][2] === 3) || 
+            (matrix[0][0] + matrix[1][1] + matrix[2][2] === 3) || 
+            (matrix[0][2] + matrix[1][1] + matrix[2][0] === 3))
+        {
+            setTimeout(function(){alert("Выиграли крестики")}, 100);
+        } 
+    else if (count == 9){
+        setTimeout(function(){alert("Ничья");}, 100);
+    } 
 }
 
-cont.addEventListener('click', function(evt){
-    coord = evt.target;
-    // console.log(coord);
-    console.log("matrix[coord.id-1]", isNaN(matrix[coord.id-1]))
-    if (isNaN(matrix[coord.id-1])){
-        count=count+1;
-        // console.log(count, "1", matrix)
-        print(coord, count);
-       
-        console.log( "4", matrix);
-        if ((matrix[0] + matrix[1] + matrix[2] === 0) ||  
-            (matrix[3] + matrix[4] + matrix[5] === 0) ||  
-            (matrix[6] + matrix[7] + matrix[8] === 0) || 
-            (matrix[0] + matrix[3] + matrix[7] === 0) || 
-            (matrix[1] + matrix[4] + matrix[7] === 0) || 
-            (matrix[2] + matrix[8] + matrix[5] === 0) || 
-            (matrix[0] + matrix[4] + matrix[8] === 0) || 
-            (matrix[2] + matrix[4] + matrix[6] === 0) )
-            {
-                setTimeout(function(){alert("Выиграли нолики")}, 100);
-            } 
-        else if ((matrix[0] + matrix[1] + matrix[2] === 3) || 
-                (matrix[3] + matrix[4] + matrix[5] === 3) || 
-                (matrix[6] + matrix[7] + matrix[8] === 3) || 
-                (matrix[0] + matrix[3] + matrix[7] === 3) || 
-                (matrix[1] + matrix[4] + matrix[7] === 3) || 
-                (matrix[2] + matrix[8] + matrix[5] === 3) ||  
-                (matrix[0] + matrix[4] + matrix[8] === 3) ||  
-                (matrix[2] + matrix[4] + matrix[6] === 3) )
-                {
-                    setTimeout(function(){alert("Выиграли крестики")}, 100);
-                } 
-        else if (count == 9){
-            setTimeout(function(){alert("Ничья");}, 100);
-        } 
-        
-    }
+// функция клика на поле
 
+function Click(event,x,y){
+    console.log(x ,y)
+    coord = event.target;
+    if (isNaN(matrix[x][y])){
+        count = count + 1;
+        print(coord, count, x, y);
+        test_win(matrix)
     }
-);
+}
 
-let again = document.querySelector(`.again`);
+// при клике на кнопку again перезагрузка страницы
 
 again.addEventListener('click', function(){
     location.reload()
